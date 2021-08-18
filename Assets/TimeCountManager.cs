76d60c6f123c2e399664e90a6e39b5cdccc9496e
kudamono83 <82087522+kudamono83@users.noreply.github.com>
2021-08-18 19:08:49 +0900

    2021/08/18 #11 時間計測

---
+++ Assets/TimeCountManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TimeCountManager : MonoBehaviour
+{
+    public float timeCount;
+    public Text countDownText;
+    public int minutes;
+    public float seconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeCount = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(WaitSignal(4, () =>
+        {
+            timeCount += Time.deltaTime;
+        }));
+            minutes = (int)timeCount / 60;
+            seconds = timeCount % 60.0f;
+
+            countDownText.text = "Time　" + minutes.ToString("0") + " : " + seconds.ToString("00.0");
+    }
+    
+    private IEnumerator WaitSignal(float waitTime, Action action) 
+    {
+        yield return new WaitForSeconds(waitTime);
+        action();
+    }
+
+}
